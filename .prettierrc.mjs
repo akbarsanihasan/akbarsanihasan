8e8b5ad9f7120e5effd988a758c7e83ae397a48f
akbarsanihasan <akbarsanihasan.pro@gmail.com>
2025-07-17 07:36:15 +0700

    update: placeholder

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,6 +2,9 @@
 /** @type {import("prettier").Config} */
 export default {
     plugins: ['prettier-plugin-astro'],
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
     overrides: [
         {
             files: '*.astro',
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+    server: {
+        port: 42069,
+        host: true,
+        allowedHosts: true,
+    },
+})
