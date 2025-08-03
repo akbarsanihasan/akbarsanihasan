e2bf3ce331e00d0aa37b6483b9d0fdeadfa99b29
akbarsanihasan <akbarsanihasan.pro@gmail.com>
2025-08-03 20:46:22 +0700

    refactor: cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://akbarsani.fun',
+    integrations: [sitemap()],
     server: {
         port: 42069,
         host: true,
         allowedHosts: true,
     },
-
-    integrations: [sitemap()],
 })
